c02948b628f03ed3f201fe2329029712048d424c
netbe <francois.benaiteau@gmail.com>
2015-06-20 18:22:31 +0200

    update sample

--- hello.py
+++ hello.py
@@ -1,9 +1,8 @@
+import os
 from flask import Flask
+
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def hello():
-    return "Hello World!"
-
-if __name__ == "__main__":
-    app.run()
+    return 'Hello World!'
