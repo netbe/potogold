c07e1124641fae9c510ed4f405a7e148d849fac7
benjamin wilson <benjamin@asmusas.net>
2015-06-21 11:45:07 +0200

    mailer small changes

--- mailer.py
+++ mailer.py
@@ -1,24 +1,20 @@
 import sendgrid
-
 import os
 
-
-
-def sendmail():
+def send_email_notification(email):
   sg = sendgrid.SendGridClient(os.environ.get('SENGRID_USERNAME'), os.environ.get('SENGRID_PASSWORD'))
   message = sendgrid.Mail()
-  message.add_to('becafoin@gmail.com')
+  message.add_to(email)
   message.set_subject('Example1')
-  message.set_text('Body')
-  message.set_from('Doe John ')
+  message.set_text('')
   # message.add_filter('templates', 'enable', '1')
-  # message.add_filter('templates', 'template_id', '8975824')
-  message.add_filter('templates', 'template_id', 'Potogold_final')
+  message.add_filter('templates', 'template_id', 'c3929ae4-4c32-4e05-a0f5-c37b5b85359a')
   # mail.add_substitution('#recipient#', emails[0][0])
   status, msg = sg.send(message)
   # rep_name
   # fix_name
   print status
   print msg
+
 if __name__ == "__main__":
   sendmail()
